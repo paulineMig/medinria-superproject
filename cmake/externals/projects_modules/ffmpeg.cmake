7e576df958d5a46b6cc5893e7b36885fb70fae56
mathildemerle <mathilde.merle@ihu-liryc.fr>
2019-02-19 16:21:49 +0100

    [ffmpeg] adapt to Windaube (#52)
    * [ffmpeg] adapt to Windaube
    
    * [ffmpeg] small coquille

--- cmake/externals/projects_modules/ffmpeg.cmake
+++ cmake/externals/projects_modules/ffmpeg.cmake
@@ -44,22 +44,40 @@ EP_SetDirectories(${ep}
 ## Define repository where get the sources
 ## #############################################################################
 
-set(tag "release/0.7") # cf. ${GITHUB_PREFIX}FFmpeg/FFmpeg.git for release numbers
-if (NOT DEFINED ${ep}_SOURCE_DIR)
-    set(location GIT_REPOSITORY "https://git.ffmpeg.org/ffmpeg.git" GIT_TAG ${tag})
+if (WIN32)
+  if (NOT DEFINED ${ep}_SOURCE_DIR)
+      set(location URL "https://ffmpeg.zeranoe.com/builds/win64/static/ffmpeg-20190217-9326117-win64-static.zip")
+  endif()
+else()
+  set(tag "release/0.7") # cf. ${GITHUB_PREFIX}FFmpeg/FFmpeg.git for release numbers
+  if (NOT DEFINED ${ep}_SOURCE_DIR)
+      set(location GIT_REPOSITORY "https://git.ffmpeg.org/ffmpeg.git" GIT_TAG ${tag})
+  endif()
 endif()
 
 ## #############################################################################
 ## Add external-project
 ## #############################################################################
-
-ExternalProject_Add(${ep}
-  ${ep_dirs}
-  ${location}
-  CONFIGURE_COMMAND ${CMAKE_CURRENT_SOURCE_DIR}/${ep}/configure --disable-yasm --disable-static --disable-network --disable-zlib --disable-ffserver --disable-ffplay --disable-decoders --enable-shared --prefix=${CMAKE_CURRENT_SOURCE_DIR}/build/${ep}/build
-  PREFIX ${CMAKE_CURRENT_SOURCE_DIR}/build/${ep}/build
-  BUILD_COMMAND make install
+if (WIN32)
+  ExternalProject_Add(${ep}
+    ${ep_dirs}
+    ${location}
+	CONFIGURE_COMMAND ""
+	INSTALL_COMMAND ""
+	BUILD_COMMAND mv ${CMAKE_CURRENT_SOURCE_DIR}/${ep}/bin/ffmpeg.exe ${CMAKE_CURRENT_SOURCE_DIR}/build/${ep}/build
   )
+else()
+  ExternalProject_Add(${ep}
+    ${ep_dirs}
+    ${location}
+    CONFIGURE_COMMAND ${CMAKE_CURRENT_SOURCE_DIR}/${ep}/configure
+	--disable-yasm --disable-static --disable-network --disable-zlib --disable-ffserver
+	--disable-ffplay --disable-decoders
+	--enable-shared --prefix=${CMAKE_CURRENT_SOURCE_DIR}/build/${ep}/build
+    PREFIX ${CMAKE_CURRENT_SOURCE_DIR}/build/${ep}/build
+    BUILD_COMMAND make install
+  )
+endif()
 
 ## #############################################################################
 ## Set variable to provide infos about the project
